528c4a7e79d42f9753615648c79c2d61b9867609
ryanapfel <rapfel@usc.edu>
2022-03-09 18:04:50 -0700

    users addition. bug fixes directory chagnes

--- excel_output.py
+++ excel_output.py
@@ -10,116 +10,39 @@ import xlsxwriter
 import re
 import click
 import configparser
+from src.extract import *
+
 
 # ESTABLISH DEFAULTS
 config = configparser.ConfigParser()
 relativePath = os.path.dirname(os.path.abspath(__file__))
 config.read(f'{relativePath}/config.cfg')
-DBPATH = config['database']['database_path']
-MASTER_LOG_DEST = config['master']['destination'] + 'master_tracker.xlsx'
-DEFAULT_STUDY_DEST = config['master']['destination']
-STDUDY_DIRS = dict(config['studyDirs'])
-
 USER = os.path.expanduser('~')
+DBPATH = USER + config['database']['database_path']
+
+MASTER_LOG_DEST = USER + config['master']['destination'] + 'master_tracker.xlsx'
+DEFAULT_STUDY_DEST = USER + config['master']['destination']
+STDUDY_DIRS =  {k: USER + v for k,v in dict(config['studyDirs']).items()}
+USER_DB = USER + config['database']['users_db']
+USER_OUTPUT = USER + config['master']['destination'] + 'user_tracker.xlsx'
 DB_PATH = f'{USER}/Documents/Horos Data/Database.sql'
-EXCEL_PATH = 'spreadsheets/master_tracker.xlsx'
 
 '''
 Returns dataframe to do work directly from hors database
 '''
-status_map = {0: 'Not Proccessed',
-              1: 'Ready For Review',
-              3: 'Issues with Imaging',
-              2: 'In Review',
-              4: 'Ajudicated'}
-
-
-def extract(path):
-    horoscon = sql.connect(path)
-    horoscur = horoscon.cursor()
-    query = """select datetime(ZDATE,'unixepoch','31 years','localtime') as acquistion_time,
-            datetime(ZDATEADDED,'unixepoch','31 years','localtime') as date_added, 
-            ZNAME as patient_name,
-            ZMODALITY as modality,
-            ZSTATETEXT as status,
-            Z_PK,
-            ZCOMMENT
-            from ZSTUDY"""
-
-    dfh = pd.read_sql(query, horoscon)
-    return dfh
-
-
-def is_int(element: any) -> bool:
-    try:
-        int(element)
-        return True
-    except ValueError:
-        return False
-
-def split_patient_name(row):
-    try:
-        split_data = re.split('[-_]', row.patient_name)
-        other = []
-        for idx, splitItem in enumerate(split_data):
-            if idx == 0:
-                row['Study'] = splitItem
-            elif idx == 1 and is_int(splitItem):
-                row['Site_id'] = int(splitItem)
-                row['Unresolved'] = False
-            elif idx == 2 and is_int(splitItem):
-                row['Subject_id'] = int(splitItem)
-                row['Unresolved'] = False
-            elif idx == 3:
-                row['Timepoint'] = splitItem
-            elif (idx == 1 or idx == 2) and not is_int(splitItem):
-                row['Unresolved'] = True
-            else:
-                other.append(splitItem)
-
-            row['Other'] = other
-    except:
-        pass
-
-    return row
-
-
-def split_date_time(row):
-    dt = datetime.strptime(row.acquistion_time, '%Y-%m-%d %H:%M:%S')
-    row['aq_date'] = dt.date()
-    row['aq_time'] = dt.time()
-    return row
-
-
-def transform(df):
-    df = df.apply(split_patient_name, axis=1)
-    df = df.apply(split_date_time, axis=1)
-    df['Status'] = df.apply(lambda x: status_map[x.status], axis=1)
-    return df[['Study', 'Site_id', 'Subject_id', 'Timepoint', 'modality', 'aq_date', 'aq_time', 'acquistion_time', 'Status', 'date_added','Unresolved','patient_name']]
-
-
-# https://xlsxwriter.readthedocs.io/example_pandas_conditional.html#ex-pandas-conditional
-def write_excel(df, file_path):
-    writer = pd.ExcelWriter(file_path, engine='xlsxwriter')
-    # Get the dimensions of the dataframe.
-    (max_row, max_col) = df.shape
-    df.to_excel(writer, sheet_name='Tracker')
-    worksheet = writer.sheets['Tracker']
-
-
-    # Set the column widths, to make the dates clearer.
-    worksheet.set_column(1, max_col, 20)
-    worksheet.autofilter(0, 0, max_row, max_col - 1)
-    writer.save()
-
-
-def load(df, file_path):
-    write_excel(df, file_path)
 
 
 @click.group()
 def cli():
     pass
+
+@cli.command()
+@click.option('--output_path', default=USER_OUTPUT)
+def users(output_path):
+    ue = UserExtract(USER_DB, output_path)
+    ue.retrieve()
+
+
 
 @cli.command()
 @click.option('--db_path', default=DBPATH,  help='Location of Databse Path for HOROS or Osirix')
